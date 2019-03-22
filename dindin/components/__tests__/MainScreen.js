6ceeb5ad3c812dd7e303f36a4212d59608d7f88c
Angie <all4ax@virginia.edu>
2019-03-21 20:50:29 -0400

    delete old files

--- dindin/components/__tests__/MainScreen.js
+++ dindin/components/__tests__/MainScreen.js
@@ -8,7 +8,7 @@ export default class MainScreen extends React.Component {
         <Text style={styles.paragraph}>
           Great you logged in
         </Text>
-        <Image style={styles.logo} source={require('../assets/images/icon.png')} />
+        <Image style={styles.logo} source={require('../assets/images/avatar.png')} />
       </View>
     );
   }
