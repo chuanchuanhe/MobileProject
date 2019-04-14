59059b19fba51c9bc58651c0c358e638f2f7ae8f
Chuanchuan He <ch9nd@virginia.edu>
2019-04-14 00:24:56 -0400

    animation and button

--- dindin/components/splash.js
+++ dindin/components/splash.js
@@ -1,93 +1,19 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity, Animated} from 'react-native'; 
+import { Dimensions, Text, View, StyleSheet, Image, TouchableOpacity, Animated,} from 'react-native'; 
 //import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import {I18n} from '../src/i18n/I18n';
 
-export default class splash extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
-    }
-  }
-  
-  componentDidMount() {
-    Animated.timing(          // Animate over time
-      this.state.fadeAnim,    // The animated value to drive
-      {
-        toValue: 1,           // Animate to opacity: 1 (opaque)
-        duration: 1500,       // 2000ms
-      }
-    ).start();                // Starts the animation
-  }
 
-  render() {
-    let { fadeAnim } = this.state;
-    return (
-    <View style={styles.container}>
-      <Animated.View View style={{ ...this.props.style, opacity: fadeAnim }}>
-        {this.props.children}
-        <Image style={styles.avatarStyle} source={require('../assets/images/avatar.png')} />
-      </Animated.View>
-      {/* <Text style={styles.title}>DinDin </Text>
-      <Text style={styles.subtitle}>connecting food lovers</Text> */}
-      <Text style={styles.title}>{I18n.t('DinDin')} </Text>
-      <Text style={styles.subtitle}>{I18n.t('connecting')}</Text>
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={() => this.props.navigation.navigate('Login')}>
-        {/*<Text> Get Started </Text> */}
-        <Text> {I18n.t('Get_Started')} </Text>
-      </TouchableOpacity> 
-    </View> 
-    ) 
-  }
-}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1, 
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    avatarStyle: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      margin: 50,
-    },
-    title: {
-      textAlign: 'center', 
-      fontWeight: 'bold',
-      fontSize: 23,
-      marginTop: 5,
-      width: 200,
-      fontFamily: 'Helvetica',
-    },
-    subtitle:{
-      color: 'grey',
-      fontStyle: 'italic',
-      fontWeight: 'normal',
-      fontSize: 15,
-      height:100,
-      alignItems: 'center',
-      marginTop: 5,
-    },
-    button: {
-      alignItems: 'center',
-      backgroundColor: 'skyblue',
-      padding: 10, 
-    }
-  })
-/** 
 export default class splash extends React.Component {
   isMounted = false;
 
   constructor(props) {
     super(props)
     this.state = {
-      avatar1 = new Animated.Value(0),
-      avatar2 = new Animated.Value(0),
-      avatar3 = new Animated.Value(0)
+      avatar1: new Animated.Value(0),
+      avatar2: new Animated.Value(0),
+      avatar3:  new Animated.Value(0), 
     }
   }
 
@@ -96,20 +22,20 @@ export default class splash extends React.Component {
       anim,    // The animated value to drive
       {
         toValue: 1,           // Animate to opacity: 1 (opaque)
-        duration: 1200,       // 2000ms
+        duration: 2000,       // 2000ms
       }
     ).start(()=>this.fadeOut1(anim));              
   }
 
   fadeOut1(anim) {
-    if (this.isMounted == true) {
+    if (this.isMounted) {
       this.setState({anim: new Animated.Value(1)})
     }
     Animated.timing(          // Animate over time
       anim,    // The animated value to drive
       {
         toValue: 0,           // Animate to opacity: 1 (opaque)
-        duration: 1200,       // 2000ms
+        duration: 1000,       // 2000ms
       }
     ).start(()=>this.fadeIn2(this.state.avatar2));  
   }
@@ -119,22 +45,22 @@ export default class splash extends React.Component {
       anim,    // The animated value to drive
       {
         toValue: 1,           // Animate to opacity: 1 (opaque)
-        duration: 1200,       // 2000ms
+        duration: 1000,       // 2000ms
       }
     ).start(()=>this.fadeOut2(anim));
   }
 
-  fadeOut1(anim) {
-    if (this.isMounted == true) {
+  fadeOut2(anim) {
+    if (this.isMounted) {
       this.setState({anim: new Animated.Value(1)})
     }
     Animated.timing(          // Animate over time
       anim,    // The animated value to drive
       {
         toValue: 0,           // Animate to opacity: 1 (opaque)
-        duration: 1200,       // 2000ms
+        duration: 1000,       // 2000ms
       }
-    ).start(()=>this.fadeIn2(this.state.avatar3));  
+    ).start(()=>this.fadeIn3(this.state.avatar3));  
   }
 
   fadeIn3(anim) {
@@ -142,44 +68,157 @@ export default class splash extends React.Component {
       anim,    // The animated value to drive
       {
         toValue: 1,           // Animate to opacity: 1 (opaque)
-        duration: 1200,       // 2000ms
+        duration: 1000,       // 2000ms
       }
     ).start(()=>this.fadeOut3(anim));
   }
 
   fadeOut3(anim) {
-    if (this.isMounted == true) {
+    if (this.isMounted) {
       this.setState({anim: new Animated.Value(1)})
     }
     Animated.timing(          // Animate over time
       anim,    // The animated value to drive
       {
         toValue: 0,           // Animate to opacity: 1 (opaque)
-        duration: 1200,       // 2000ms
+        duration: 1000,       // 2000ms
       }
-    ).start();  
+    ).start();  //startthe animation
   }
 
   componentWillMount() {
-    this.fadeIn1(this.state.avatar1)
+    this.fadeIn1(this.state.avatar1);
   }
 
   componentDidMount() {
-    this.isMounted = true
-    this.timerId = setInterval(()=>this.fadeIn1(this.state.avatar1))
+    this.isMounted = true;
+    this.timerId = setInterval(()=>this.fadeIn1(this.state.avatar1), 6000);
   }
 
-  componentWillMount() {
-    this.isMounted = false
+  componentWillUnmount() {
+    this.isMounted = false; 
   }
 
   render() {
-    let {avatar1} = this.state;
-    let {}
-  }
+    let { avatar1 } = this.state;
+    let { avatar2 } = this.state;
+    let { avatar3 } = this.state;
     return (
-
-    )
+    <View style={styles.container}>
+        <View style={styles.images}>
+        <Image style={styles.OvalBackground} source={require('../assets/images/Ovals.png')}/>
+        <Animated.Image style={[styles.people1, {opacity:avatar1}]} source={require('../assets/images/Page1.png')}/>
+        <Animated.Image style={[styles.people2, {opacity:avatar2}]} source={require('../assets/images/Page2.png')}/>
+        <Animated.Image style={[styles.people3, {opacity:avatar3}]} source={require('../assets/images/Page3.png')}/>
+        </View>
+        <View style={styles.title}>
+          <Text style={styles.dindin}>{I18n.t('DinDin')} </Text>
+          <Text style={styles.subtitle}>{I18n.t('connecting')}</Text>
+      </View>
+      <View style={styles.start}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+          <Image style={styles.buttonBar} source={require('../assets/images/getStarted.png')}/>
+          
+        </TouchableOpacity> 
+      </View>
+    </View> 
+    ) 
+  }
 }
+//<View style={styles.textView}><Text style={styles.buttonText}>{I18n.t('Get_Started')} </Text></View>
+  var windowWidth = Dimensions.get('window').width;
+  var windowHeight = Dimensions.get('window').height; 
 
-**/
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1, 
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    images: {
+      top:90,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    OvalBackground: {
+      width: 259,
+      height: 259,
+    },
+
+    title: {
+      //textAlign: 'center', 
+      //fontWeight: 'bold',
+      //fontSize: 23,
+      //marginTop: 5,
+      //width: 200,
+      //fontFamily: 'Helvetica',
+      bottom:60
+    },
+
+    dindin:{
+      fontFamily: 'System',
+      fontSize:29,
+      textAlign: 'center'
+    },
+
+    subtitle:{
+      //color: 'grey',
+      fontFamily: 'System',
+      fontStyle: 'italic',
+      fontWeight: 'normal',
+      fontSize: 15,
+      height:100,
+      alignItems: 'center',
+      marginTop: 5,
+      paddingTop: 8
+    },
+
+    start: {
+      position: 'absolute',
+      alignItems: 'center',
+      justifyContent: 'center',
+      bottom: 0,
+      height: 0.076*windowHeight,
+    },
+    textView:{
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center', 
+    },
+    buttonBar: {
+      width: windowWidth,
+      height: 0.076*windowHeight,
+    },
+    buttonText: {
+      fontFamily: 'System',
+      fontSize:14,
+      textAlign:'center',
+    },
+
+    people1:{
+      width:54,
+      height:57.18,
+      left:60,
+      bottom:60
+    },
+
+    people2: {
+      width:67,
+      height: 72,
+      left:60, 
+      bottom: 300,
+    },
+
+    people3:{
+      width:73,
+      height:80,
+      right:90,
+      bottom:300,
+    },
+  })
